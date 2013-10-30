b0541e7d4b0b1861f3c67a090e240ad056475bb6
Colin Dean <git@cad.cx>
2013-10-30 00:23:44 -0400

    Remove unnecessary tests
    No sense in testing
    * simple mutators and accessors,
    * factory methods
    
    I should eventually test the execution method...

--- src/test/java/cx/cad/nfsn/APITest.java
+++ src/test/java/cx/cad/nfsn/APITest.java
@@ -1,13 +1,6 @@
 package cx.cad.nfsn;
 
-import cx.cad.nfsn.objects.Account;
-import cx.cad.nfsn.objects.Database;
 import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 public class APITest {
 
@@ -16,49 +9,6 @@ public class APITest {
     @Before
     public void setUp() throws Exception {
         this.api = new API("user", "key", true);
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testAPI() {
-        assertTrue(this.api.getClass().equals(API.class));
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetAccount() {
-        assertTrue(this.api.getAccount("account").getClass().equals(Account.class));
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetDatabase() {
-        assertTrue(this.api.getDatabase("database").getClass().equals(Database.class));
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetDNS() {
-        fail("Not yet implemented");
-
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetEmail() {
-        fail("Not yet implemented");
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetMember() {
-        fail("Not yet implemented");
-    }
-
-    @Ignore("Not yet implemented")
-    @Test
-    public void testGetSite() {
-        fail("Not yet implemented");
     }
 
 }
