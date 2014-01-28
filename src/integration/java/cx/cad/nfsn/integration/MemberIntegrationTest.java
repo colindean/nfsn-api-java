5fc79c5b9e7271ae06570964de8aef2d652983fe
Colin Dean <git@cad.cx>
2014-01-27 21:00:11 -0500

    Basic Member integration tests working, too!

--- src/integration/java/cx/cad/nfsn/integration/MemberIntegrationTest.java
+++ src/integration/java/cx/cad/nfsn/integration/MemberIntegrationTest.java
@@ -14,6 +14,8 @@ import org.junit.Test;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class MemberIntegrationTest {
 
@@ -27,24 +29,33 @@ public class MemberIntegrationTest {
     @Before
     public void before() throws IOException {
         properties = TestingProperties.loadTestingProperties();
-
         API api = new API(properties.getProperty("integration.login"), properties.getProperty("integration.key"), API.DEBUG_MODE);
         member = api.getMember(properties.getProperty("integration.member.username"));
-
-
     }
 
-    @Ignore(value = "Awaiting key")
+    @Ignore(value = "Awaiting NFSN implementation")
     @Betamax(tape = "cx.cad.nfsn.integration.member.getEmail")
     @Test
     public void testGetEmail() throws APIObject.NFSNNotYetImplementedException {
         assertEquals(properties.getProperty("integration.member.email"), member.getEmail());
     }
 
-    @Ignore(value = "Awaiting key")
+    @Ignore(value = "Awaiting NFSN implementation")
     @Betamax(tape = "cx.cad.nfsn.integration.member.getStatus")
     @Test
     public void testGetStatus() throws APIObject.NFSNNotYetImplementedException {
         assertEquals(properties.getProperty("integration.member.status"), member.getStatus());
+    }
+
+    @Betamax(tape="cx.cad.nfsn.integration.member.getSites")
+    @Test
+    public void testGetSites(){
+        assertThat(member.getSites(), hasSize(Integer.parseInt(properties.getProperty("integration.member.numSites"))));
+    }
+
+    @Betamax(tape="cx.cad.nfsn.integration.member.getAccounts")
+    @Test
+    public void testGetAccounts(){
+        assertThat(member.getAccounts(), hasSize(Integer.parseInt(properties.getProperty("integration.member.numAccounts"))));
     }
 }
